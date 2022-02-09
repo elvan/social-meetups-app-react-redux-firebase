8c6625347e70f2cfd4bcdf9e13a96452512c7b5c
Elvan H <elvanhd@gmail.com>
2022-02-09 20:57:10 +0700

    navigation bar and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Container } from 'react-bootstrap';
 import { MeetupDashboard } from './features/meetups/meetup-dashboard/MeetupDashboard';
+import { AppNavbar } from './layout/AppNavbar';
 
 export const App = () => {
   return (
-    <Container fluid='lg' className='vh-100'>
-      <h1>SocialMeetups</h1>
-      <MeetupDashboard />
-    </Container>
+    <div className='vh-100'>
+      <AppNavbar />
+      <Container fluid='lg'>
+        <MeetupDashboard />
+      </Container>
+    </div>
   );
 };
---
+++ src/layout/AppNavbar.jsx
@@ -0,0 +1,38 @@
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+export const AppNavbar = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      className='mb-3'
+      expand='md'
+      bg='dark'
+      variant='dark'
+    >
+      <Container fluid='lg'>
+        <Navbar.Brand href='/'>SocialMeetups</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/meetups'>All Meetups</Nav.Link>
+            <Nav.Link href='/create-meetup'>Create Meetup</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href='/login'>Login</Nav.Link>
+            <Nav.Link href='/register'>Register</Nav.Link>
+            <NavDropdown
+              alignRight={true}
+              title='user@example.com'
+              id='collasible-nav-dropdown'
+            >
+              <NavDropdown.Item href='/profile'>Profile</NavDropdown.Item>
+              <NavDropdown.Item href='/settings'>Settings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='/logout'>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
