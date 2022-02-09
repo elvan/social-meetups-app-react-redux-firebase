df61ba739e372a2c880468af96c61b705cb1451b
Elvan H <elvanhd@gmail.com>
2022-02-09 19:34:03 +0700

    using the bootstrap button and spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { Button, Container, Spinner } from 'react-bootstrap';
+
 export const App = () => {
-  return <h1>SocialMeetups</h1>;
+  return (
+    <Container fluid='lg' className='vh-100'>
+      <h1>SocialMeetups</h1>
+      <div>
+        <Button variant='dark' disabled>
+          <Spinner animation='border' as='span' className='mr-2' size='sm' />
+          Loading...
+        </Button>
+      </div>
+    </Container>
+  );
 };
