14c6140a94ed3913df182fe0488059798b6a1961
Elvan H <elvanhd@gmail.com>
2022-02-10 22:01:22 +0700

    configuring redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { testReducer } from '../features/sandbox/store/testReducer';
 
 export const configureStore = () => {
-  return createStore(testReducer);
+  return createStore(testReducer, devToolsEnhancer({}));
 };
