6069200e548c71f283ee6472550d309f59ad262f
Elvan H <elvanhd@gmail.com>
2022-02-12 14:34:39 +0700

    creating async reducer and actions

---
+++ src/common/async/asyncActions.js
@@ -0,0 +1,24 @@
+import {
+  ASYNC_ACTION_ERROR,
+  ASYNC_ACTION_FINISH,
+  ASYNC_ACTION_START,
+} from './asyncConstants';
+
+export const asyncActionStart = () => {
+  return {
+    type: ASYNC_ACTION_START,
+  };
+};
+
+export const asyncActionFinish = () => {
+  return {
+    type: ASYNC_ACTION_FINISH,
+  };
+};
+
+export const asyncActionError = (error) => {
+  return {
+    type: ASYNC_ACTION_ERROR,
+    payload: error,
+  };
+};
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { asyncReducer } from '../common/async/asyncReducer';
 import { modalReducer } from '../components/modals/store/modalReducer';
 import { authReducer } from '../features/auth/store/authReducer';
 import { meetupReducer } from '../features/meetups/store/meetupReducer';
 import { testReducer } from '../features/sandbox/store/testReducer';
 
 export const rootReducer = combineReducers({
+  asyncState: asyncReducer,
   authState: authReducer,
   meetupState: meetupReducer,
   modalState: modalReducer,
