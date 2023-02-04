2236fe3abf805b85b479b3940b5d3024694cfc71
Elvan Hidayat <elvanhd@gmail.com>
2023-02-04 09:25:30 +0700

    default photo url

--- src/features/meetups/components/MeetupListAttendee.jsx
+++ src/features/meetups/components/MeetupListAttendee.jsx
@@ -5,10 +5,10 @@ export const MeetupListAttendee = ({ attendee }) => {
   return (
     <Link to={`/profiles/${attendee.id}`}>
       <Image
-        src={attendee.photoURL}
+        src={attendee.photoURL || '/assets/user.png'}
         width={50}
         roundedCircle
-        className='mr-1'
+        className="mr-1"
         style={{ height: '50px', width: '50px' }}
       />
     </Link>
--- src/features/meetups/components/MeetupListItem.jsx
+++ src/features/meetups/components/MeetupListItem.jsx
@@ -28,79 +28,79 @@ export const MeetupListItem = ({ meetup }) => {
   }
 
   return (
-    <Card className='mb-3 rounded bg-white shadow'>
-      <Card.Body className='row clearfix py-3 px-4'>
+    <Card className="mb-3 rounded bg-white shadow">
+      <Card.Body className="row clearfix py-3 px-4">
         <Image
-          className='float-left ml-2 mr-2'
-          src={meetup.hostPhotoURL}
+          className="float-left ml-2 mr-2"
+          src={meetup.hostPhotoURL || '/assets/user.png'}
           roundedCircle
           style={{ height: '100px', width: '100px' }}
         />
         <Col>
-          <Card.Title className='mb-2'>{meetup.title}</Card.Title>
+          <Card.Title className="mb-2">{meetup.title}</Card.Title>
           <Card.Text>
             Hosted by{' '}
             <Link to={`/profiles/${meetup.hostUid}`}>{meetup.hostedBy}</Link>
           </Card.Text>
         </Col>
       </Card.Body>
-      <ListGroup className='list-group-flush'>
+      <ListGroup className="list-group-flush">
         <ListGroupItem>
           <Row>
             <Col>
-              <div className='d-flex align-items-center'>
-                <FaCalendar size={15} className='mr-2' />
+              <div className="d-flex align-items-center">
+                <FaCalendar size={15} className="mr-2" />
                 {format(meetup.date, 'MMMM d, yyyy h:mm a')}
               </div>
             </Col>
             <Col>
-              <div className='d-flex align-items-center'>
-                <FaTag size={15} className='ml-3 mr-2' />
+              <div className="d-flex align-items-center">
+                <FaTag size={15} className="ml-3 mr-2" />
                 {meetup.category}
               </div>
             </Col>
           </Row>
         </ListGroupItem>
         <ListGroupItem>
-          <div className='d-flex align-items-center'>
-            <FaMapMarkerAlt size={15} className='mr-2' />
+          <div className="d-flex align-items-center">
+            <FaMapMarkerAlt size={15} className="mr-2" />
             {meetup.venue.address}
           </div>
         </ListGroupItem>
         {meetup.attendees.length > 0 ? (
-          <ListGroupItem className='bg-light py-2'>
+          <ListGroupItem className="bg-light py-2">
             {meetup.attendees.map((attendee) => (
               <MeetupListAttendee key={attendee.id} attendee={attendee} />
             ))}
           </ListGroupItem>
         ) : (
-          <ListGroupItem className='bg-light py-2'>
-            <div className='d-flex align-items-center'>
-              <FaUsers size={15} className='mr-2' />
+          <ListGroupItem className="bg-light py-2">
+            <div className="d-flex align-items-center">
+              <FaUsers size={15} className="mr-2" />
               No attendees yet
             </div>
           </ListGroupItem>
         )}
         <ListGroupItem>{meetup.description}</ListGroupItem>
-        <ListGroupItem className='bg-light'>
-          <div className='d-flex align-items-center'>
-            <Link to={`/meetups/${meetup.id}`} className='btn btn-info mr-2'>
-              <div className='d-flex align-items-center'>
-                <FaUsers size={15} className='mr-2' />
+        <ListGroupItem className="bg-light">
+          <div className="d-flex align-items-center">
+            <Link to={`/meetups/${meetup.id}`} className="btn btn-info mr-2">
+              <div className="d-flex align-items-center">
+                <FaUsers size={15} className="mr-2" />
                 View Meetup
               </div>
             </Link>
             {false && (
-              <Button className='btn btn-danger mr-2' onClick={handleDelete}>
-                <div className='d-flex align-items-center'>
-                  <FaTrashAlt size={15} className='mr-2' />
+              <Button className="btn btn-danger mr-2" onClick={handleDelete}>
+                <div className="d-flex align-items-center">
+                  <FaTrashAlt size={15} className="mr-2" />
                   Delete
                 </div>
               </Button>
             )}
             {meetup.isCancelled && (
               <span
-                className='badge badge-danger ml-auto'
+                className="badge badge-danger ml-auto"
                 style={{ fontSize: '1rem' }}
               >
                 Cancelled
